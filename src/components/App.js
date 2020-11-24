4ef4f51c109badafdc892575c3584793a286d0f6
surajsinha2512 <61259615+surajsinha2512@users.noreply.github.com>
2020-11-24 20:30:40 +0530

    routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React, {Component, useState} from "react";
+import { useLocation ,BrowserRouter, Switch, Router,Route, Link} from "react-router-dom";
 import '../styles/App.css';
 
 class App extends Component {
@@ -6,10 +7,43 @@ class App extends Component {
 
         return(
             <div id="main">
-               {/* Do not remove the main div */}
+               {/* Do not remove the main div */} 
+               <BrowserRouter>
+               <LocationDisplay/>
+               
+               <Link to="/">Home</Link>
+               <Link to="/about">About</Link>
+               <Switch>
+               <Route exact path="/">
+        
+               <Home/>
+               </Route>
+               <Route exact path="/about">
+               <About/>
+               </Route>
+               <Route path="*">
+               <Match/>
+               </Route>
+               </Switch>
+               </BrowserRouter> 
             </div>
         )
     }
+   
+}
+function Home(){
+    return <div>You are home</div>
+}
+function About(){
+    return <div>You are on the about page</div>
+}
+function LocationDisplay(){
+   
+  //  const {pathname}=useLocation();
+return <div data-testid="location-display">{useLocation().pathname}</div>
+}
+function Match(){
+    return <div>No match</div>
 }
 
 
